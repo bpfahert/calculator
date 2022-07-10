977ed273c0a0ba7d7d6c20feb972e570db5f1807
Bryan <bpfahert@gmail.com>
2022-07-10 13:30:58 -0500

    Create index page, css stylesheet, and js script
    A basic html skeleton, css stylesheet, and javascript file were created. Basic math functions were added to script.js.

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let previousResult= 0;
+
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
